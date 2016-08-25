28dae6499af35958a5b7a2367f29a4ba8aca6311
Hack Reactor Students <accounts@hackreactor.com>
2016-08-25 11:33:41 -0700

    Refactored to mangoDB

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var app = require('./server-config.js');
+var morgan = require('morgan');
+
 
 var port = process.env.PORT || 4568;
-
+app.use(morgan('dev'));
 app.listen(port);
 
 console.log('Server now listening on port ' + port);
